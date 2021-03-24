1a0e309176e15e532f8b29b0b1f0eb1b1110542f
Fuinha11 <marcosdgmarques@gmail.com>
2021-03-24 15:04:59 -0300

    Add comments to first_scrape.py

--- first_scrape.py
+++ first_scrape.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 
 def do_thing():
     url = "https://alura-site-scraping.herokuapp.com/hello-world.php"
-    response = urlopen(url).read()
-    soup = BeautifulSoup(response, 'html.parser')
-    print(soup.find('h1', id='hello-world').get_text())
+    response = urlopen(url) # Get on url
+    html = response.read() # Read the html data from response
+    soup = BeautifulSoup(html, 'html.parser') # Parse HTML to a Beautiful Soup OBJ
+    print(soup.find('h1', id='hello-world').get_text()) # Find the desired tag
     print(soup.find('p').get_text())
     
 
